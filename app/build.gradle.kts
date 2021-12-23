0f25dd0d3868cc9f98c546e5d67ce23062d70d30
Ермолаев Архип Федотович <arkhip.ermolaev@mail.ru>
2021-12-23 20:44:51 +0900

    Kotlin-DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+android {
+
+    compileSdk = 31
+    buildToolsVersion = "31.0.0"
+
+    defaultConfig {
+        applicationId = "com.umbrella.mytranslator"
+        minSdk = 24
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(Dependencies.CORE_KTX_DEP)
+    implementation(Dependencies.APP_COMPAT_DEP)
+    implementation(Dependencies.MATERIAL_DEP)
+    implementation(Dependencies.CONSTRAINT_LAYOUT_DEP)
+    implementation(Dependencies.LEGACY_SUPPORT_DEP)
+    testImplementation(Dependencies.J_UNIT_DEP)
+    androidTestImplementation(Dependencies.TEST_EXT_J_UNIT_DEP)
+    androidTestImplementation(Dependencies.ESPRESSO_CORE_DEP)
+
+    //retrofit
+    implementation(Dependencies.RETROFIT_DEP)
+    implementation(Dependencies.RETROFIT_CONVERTER_GSON_DEP)
+
+    //picasso
+    implementation(Dependencies.PICASSO_DEP)
+
+    //koin
+    implementation(Dependencies.KOIN_CORE_DEP)
+    implementation(Dependencies.KOIN_ANDROID_DEP)
+
+    //coroutines
+    implementation(Dependencies.LIFECYCLE_VIEWMODEL_KTX_DEP)
+
+    //Room
+    implementation(Dependencies.ROOM_RUNTIME_DEP)
+    kapt(Dependencies.ROOM_COMPILER_DEP)
+    implementation(Dependencies.ROOM_KTX_DEP)
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,50 @@
+object Versions {
+    const val CORE_KTX_VERSION = "1.7.0"
+    const val APP_COMPAT_VERSION = "1.4.0"
+    const val MATERIAL_VERSION = "1.4.0"
+    const val CONSTRAINT_LAYOUT_VERSION = "2.1.2"
+    const val LEGACY_SUPPORT_VERSION = "1.0.0"
+    const val J_UNIT_VERSION = "4.13.2"
+    const val TEST_EXT_J_UNIT_VERSION = "1.1.3"
+    const val ESPRESSO_CORE_VERSION = "3.4.0"
+    const val RETROFIT_VERSION = "2.9.0"
+    const val PICASSO_VERSION = "2.71828"
+    const val KOIN_VERSION = "3.1.3"
+    const val LIFECYCLE_VIEWMODEL_KTX_VERSION = "2.4.0"
+    const val ROOM_VERSION = "2.4.0"
+}
+
+object Dependencies {
+    const val CORE_KTX_DEP = "androidx.core:core-ktx:${Versions.CORE_KTX_VERSION}"
+    const val APP_COMPAT_DEP = "androidx.appcompat:appcompat:${Versions.APP_COMPAT_VERSION}"
+    const val MATERIAL_DEP = "com.google.android.material:material:${Versions.MATERIAL_VERSION}"
+    const val CONSTRAINT_LAYOUT_DEP =
+        "androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINT_LAYOUT_VERSION}"
+    const val LEGACY_SUPPORT_DEP =
+        "androidx.legacy:legacy-support-v4:${Versions.LEGACY_SUPPORT_VERSION}"
+    const val J_UNIT_DEP = "junit:junit:${Versions.J_UNIT_VERSION}"
+    const val TEST_EXT_J_UNIT_DEP = "androidx.test.ext:junit:${Versions.TEST_EXT_J_UNIT_VERSION}"
+    const val ESPRESSO_CORE_DEP =
+        "androidx.test.espresso:espresso-core:${Versions.ESPRESSO_CORE_VERSION}"
+
+    //retrofit
+    const val RETROFIT_DEP = "com.squareup.retrofit2:retrofit:${Versions.RETROFIT_VERSION}"
+    const val RETROFIT_CONVERTER_GSON_DEP =
+        "com.squareup.retrofit2:converter-gson:${Versions.RETROFIT_VERSION}"
+
+    //picasso
+    const val PICASSO_DEP = "com.squareup.picasso:picasso:${Versions.PICASSO_VERSION}"
+
+    //koin
+    const val KOIN_CORE_DEP = "io.insert-koin:koin-core:${Versions.KOIN_VERSION}"
+    const val KOIN_ANDROID_DEP = "io.insert-koin:koin-android:${Versions.KOIN_VERSION}"
+
+    //coroutines
+    const val LIFECYCLE_VIEWMODEL_KTX_DEP =
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.LIFECYCLE_VIEWMODEL_KTX_VERSION}"
+
+    //Room
+    const val ROOM_RUNTIME_DEP = "androidx.room:room-runtime:${Versions.ROOM_VERSION}"
+    const val ROOM_COMPILER_DEP = "androidx.room:room-compiler:${Versions.ROOM_VERSION}"
+    const val ROOM_KTX_DEP = "androidx.room:room-ktx:${Versions.ROOM_VERSION}"
+}
